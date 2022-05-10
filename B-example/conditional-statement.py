98241013026efe4f86d2cab6821d0673fc6a3b55
babita93 <babitasmaurya@gmail.com>
2022-05-10 08:58:58 +0530

    condition example

---
+++ B-example/conditional-statement.py
@@ -0,0 +1,26 @@
+# if statement
+#condition is true =we can get output
+#condition is false =we get errore
+'''a=int(input("enter the even no."))
+if a%2==0:
+    print(a,"even Number")
+# IF ELSE STATEMENT
+a=int(input("enter the even no."))
+if a%2==0:
+    print(a,"even Number")
+else:
+       print(a,"even Number")''' 
+ #IF ELIF ELSE STATEMENT
+per=int(input("Value")) 
+if per>60:
+    print("first")
+elif per>48:
+    print("second")
+elif per>35:
+    print("third")
+if per<35:
+    print("fail")
+        
+
+
+
