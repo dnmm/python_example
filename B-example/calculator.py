a6fe9b9b8f8f1435ed5a6d9b89d3622504c457a9
babita93 <babitasmaurya@gmail.com>
2022-05-10 08:58:31 +0530

    calculator example

---
+++ B-example/calculator.py
@@ -0,0 +1,14 @@
+NUM1 =int(input("ENTER THE VALUE 1:-"))
+NUM2 =int(input("ENTER THE VALUE 2:-"))
+OPR =input("ENTER THE OPRATION(+,_,*,/)")
+if OPR == "+" :
+    print(NUM1+ NUM2)
+elif OPR == "-" :
+    print(NUM1- NUM2)
+elif OPR == "*" :
+    print(NUM1* NUM2)
+elif OPR == "/" :
+    print(NUM1/ NUM2)
+else:
+    print("invalid no.....")            
+    
