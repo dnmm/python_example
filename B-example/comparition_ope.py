8b90f6a86799371990c2461f7a65a5b0c9c7155f
babita93 <babitasmaurya@gmail.com>
2022-05-13 13:54:12 +0530

    Update comparition_ope.py

--- B-example/comparition_ope.py
+++ B-example/comparition_ope.py
@@ -1,6 +1,3 @@
-
-
-
 x=10
 y=20
 print(x==y)
@@ -10,4 +7,5 @@ print(x<y)
 print(x>=y)
 print(x<=y)
 
-
+'''These operators compare the values on either sides of them and decide 
+the relation among them. They are also called Relational operators.'''
