5a221a5457cd0d411f63dec502c4f12468fd23b9
vishalsinghvns <101693250+vishalsinghvns@users.noreply.github.com>
2022-05-13 12:54:54 +0530

    new example

---
+++ v_example/comment.py
@@ -0,0 +1,13 @@
+#single line comment 
+print("single line comment")
+
+#This is a comment
+#written in
+
+print("multiline comment")
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
