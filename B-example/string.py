80c1c762a1adc6b9012faec68e8ea6586e6bcfc1
babita93 <babitasmaurya@gmail.com>
2022-05-13 13:55:48 +0530

    Update string.py

--- B-example/string.py
+++ B-example/string.py
@@ -1,5 +1,25 @@
-
+#simple string
 print("identification")
 a= "Hi"
 b= "I am Babita Maurya"
 print (a+" " +b)
+
+
+var1 = 'Hello World!'
+var2 = "Python Programming"
+
+print ("var1[0]: ", var1[0])
+print ("var2[1:5]: ", var2[1:5])
+
+#Updating Strings
+var1 = 'Hello World!'
+print ("Updated String :- ", var1[:6] + 'Python')
+
+#Python String capitalize() method returns a copy of the string with only its first character capitalized.
+str = ("this is string example....wow!!!")
+print ("str.capitalize() : ", str.capitalize())
+
+#Python string method center() returns centered in a string of length width.
+#  Padding is done using the specified fillchar. Default filler is a space      
+# str.center(width[, fillchar])
+print ("str.center(40, 'a') : ", str.center(40, 'a'))
