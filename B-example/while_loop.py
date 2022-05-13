478e36b753ec53995da204da4d071a9064a3b646
babita93 <babitasmaurya@gmail.com>
2022-05-13 13:55:53 +0530

    Create while_loop.py

---
+++ B-example/while_loop.py
@@ -0,0 +1,20 @@
+#A while loop statement in Python programming language repeatedly executes a target statement as long as a given condition is true.
+count = 0
+while (count < 9):
+   print ('The count is:', count)
+   count = count + 1
+   
+   
+   
+   #Using else Statement with While Loop
+count = 0
+while count < 5:
+   print( count, " is  less than 5")
+   count = count + 1
+else:
+   print( count, " is not less than 5")
+
+
+
+
+
