3a399b8e70b5875eaf607b66e7de962c4ea3c0c1
babita93 <babitasmaurya@gmail.com>
2022-05-13 13:55:05 +0530

    Update identify_ope.py

--- B-example/identify_ope.py
+++ B-example/identify_ope.py
@@ -1,3 +1,4 @@
+#Identity operators compare the memory locations of two objects#
 #is = return true if both variable are the same object x=y
 #is not =return true if both variable are not the same object x is not y
 x=10
