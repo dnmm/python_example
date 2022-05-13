74fd3707c2f835b6fcc43f44648b53b01cb95995
babita93 <babitasmaurya@gmail.com>
2022-05-13 13:54:57 +0530

    Create for_loop.py

---
+++ B-example/for_loop.py
@@ -0,0 +1,32 @@
+#for loop 
+#for letter in 'Python':     # First Example
+   #print('Current Letter :', letter)
+
+#fruits = ['banana', 'apple',  'mango']
+#for fruit in fruits:        # Second Example
+ #  print ('Current fruit :', fruit)
+
+
+   #for loop with range
+   #range(5)
+   #start =0
+   #condition =< 5
+   #increment =1
+
+
+   # range(1,5)
+   #start =1
+   #condition =< 5
+   #increment =1
+
+
+   # range(1,6,2)
+   #start =1
+   #condition =< 6
+   #increment =2
+
+'''for n in range(5):
+    print(n)'''
+
+for a in range(1,11):
+    print("2*",a,"=",2*a)
